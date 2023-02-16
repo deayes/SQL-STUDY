cd4ccb9821e721c5a364642edca50d69657886f9
deayes <125602375+deayes@users.noreply.github.com>
2023-02-16 16:55:05 -0300

    Add files via upload

---
+++ TABLE CLIENTE.sql
@@ -0,0 +1,17 @@
+CREATE TABLE tbcliente (
+  `CPF` varchar(11) ,
+  `NOME` varchar(100) ,
+  `ENDERECO1` varchar(150) ,
+  `ENDERECO2` varchar(150) ,
+  `BAIRRO` varchar(50) ,
+  `CIDADE` varchar(50) ,
+  `ESTADO` varchar(50) ,
+  `CEP` varchar(8) ,
+  `IDADE` smallint ,
+  `SEXO` varchar(1) ,
+  `LIMITE_CREDITO` float ,
+  `VOLUME_COMPRA` float ,
+  `PRIMEIRA_COMPRA` bit(1) ,
+  `DATA_NASCIMENTO` date ,
+  PRIMARY KEY (`CPF`)
+);
